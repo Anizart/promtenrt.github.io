d487192f6b1a16cc394d1c20af705f8dd25c5cf6
Anizart <150341964+Anizart@users.noreply.github.com>
2024-09-05 17:54:54 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
         items: 1,
     });
 });
+
+$(window).scroll(() =>{
+    const scrollTop = (document).documentElement.scrollTop;
+
+    if (scrollTop >= 10) {
+      $('header').addClass('bg-blure');
+    } else {
+      $('header').removeClass('bg-blure');
+    }
+    
+});
+
+// const header = document.querySelector('.header');
+
+// window.addEventListener('scroll', () => {
+//     const scrollTop = document.documentElement.scrollTop;    
+
+//     if (scrollTop >= 10) {
+//       header.classList.add('bg-blure');
+//     } else {
+//       header.classList.remove('bg-blure');
+//     }
+// });
