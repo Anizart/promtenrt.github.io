7119f0163b63074d4a9f8c89b27c8fbaa813502a
Anizart <150341964+Anizart@users.noreply.github.com>
2024-09-10 21:37:08 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -43,3 +43,73 @@ $(window).scroll(() =>{
 //       header.classList.remove('bg-blure');
 //     }
 // });
+
+const btn = $('.button-first'),
+      btnAnime = $('[data-anime]'),
+      cards = $('.cards__card'),
+      number = cards.eq(1).find('.cards__number');
+      
+
+let i = 2;
+function changeNumber() {
+  number.html(`0${++i}`);
+
+  if (i == 5) {
+    btn.off('click', changeNumber);
+  }
+}
+
+btn.on('click', changeNumber);
+
+
+
+const div = document.createElement('div');
+document.querySelector('.first .container').prepend(div);
+const card = document.querySelectorAll('.cards__card');
+
+card.forEach((element, i) => {
+  element.addEventListener('click', clickedCard);
+
+  function clickedCard() {
+    deleteClass();
+
+    switch (i) {
+      case 0: 
+        collectingAnimations()
+        break;
+      case 1:
+        collectingAnimations()
+        break;
+    }
+  }
+  
+  function collectingAnimations() {
+    
+    div.classList.add('anime');
+    div.style.cssText = `
+                          animation-duration: 5s;
+                          animation-name: cube-${i};
+                        `
+  }
+
+  function deleteClass() {
+    div.classList.remove('anime');
+  }
+
+});
+
+// const btn = document.querySelector('.button-first'),
+//       cards = document.querySelectorAll('.cards__card'),
+//       number = cards[1].querySelector('.cards__number');      
+
+// let i = 2;
+// function changeNumber() {
+//   number.innerHTML = `0${++i}`;
+
+//   if (i == 5) {
+//     btn.removeEventListener('click', changeNumber);
+//   }
+// }
+
+// btn.addEventListener('click', changeNumber);
+
